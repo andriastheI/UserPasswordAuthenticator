5585b5bf96c9ac538c5ec7650a81e64f07141d98
Andrias Zelele <andriasmekonnenzelele@gmail.com>
2025-03-31 13:26:34 -0600

    added a new code

--- UPA/src/UPA/UserPassAuth.java
+++ UPA/src/UPA/UserPassAuth.java
@@ -1,21 +1,48 @@
 package UPA;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
+/**
+ * the way I am going to implement the methods is not that complicated
+ * I am going to make the encryptPassword method with an encryption algorithm.
+ * Algorithm:
+ * 1, from a string of letters and symbols make a random iteration and form a key
+ * 2, from the key go through the letter and find corresponding value of that letter in the
+ * string of letters that was created and get the index, then look for the element in the
+ * key and append them as a string.(making the password"
+ * 3. store the username and the key in a hashmap
+ *
+ */
 public class UserPassAuth {
     private String username;
-    private String password;
+    private String pwd;
+    private final Map<String,String>  storage = new HashMap<>();
+    private final String store = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:',.<>?/`~";
+    private String key;
+    private Random rand = new Random();
     UserPassAuth(){
+        loadUsers();
     }
 
-    private void encryptPassword(String password){
-        char[] letters = password.toCharArray();
-        char[] pass = new char[password.length()];
-        for(char letter : letters){
-            letter+=5;
-            pass[password.length()-1]=letter;
+    private void loadUsers() {
+    }
+
+    private void encryptPassword(String username, String password){
+        key = "";
+        for(int i = 0; i <= store.length(); i++){
+            char letter = store.charAt(rand.nextInt(store.length()));
+            key += letter;
         }
-        System.out.println(Arrays.toString(pass));
+        pwd= "";
+        for (char c : password.toCharArray()){
+            int ind = store.indexOf(c);
+            char let = key.charAt(ind);
+            pwd += let;
+        }
+        storage.put(username, key);
     }
 
 
@@ -24,6 +51,6 @@ public class UserPassAuth {
 
     public static void main(String[] args) {
         UserPassAuth auth = new UserPassAuth();
-        auth.encryptPassword("password");
+        auth.encryptPassword("ninja","password");
     }
 }
